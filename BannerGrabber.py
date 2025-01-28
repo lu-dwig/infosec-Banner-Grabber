2f06551ddcb78442a71e8582bf38ef3a480475c7
lu-dwig <murimiludwig@gmail.com>
2025-01-28 21:31:14 +0300

    creation of a banner for testing

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 
+import socket
+
+s = socket.socket()
+
+ip = input("Please enter the IP: ")
+port = input("Please enter the port: ")
+    
+s.connect((ip, port))
+print(s.recv(1024))
+
