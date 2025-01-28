90767fdfac7d4413557af77978824cc45b626163
lu-dwig <murimiludwig@gmail.com>
2025-01-28 21:34:23 +0300

    creation of a banner for testing  updating

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -5,8 +5,8 @@ import socket
 s = socket.socket()
 
 ip = input("Please enter the IP: ")
-port = input("Please enter the port: ")
+port = str(input("Please enter the port: "))
     
-s.connect((ip, port))
+s.connect((ip, int(port)))
 print(s.recv(1024))
 
