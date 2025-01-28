c1f6c6d2e553547db2fc4ba4ac6a627ca1c806c1
lu-dwig <murimiludwig@gmail.com>
2025-01-28 21:52:30 +0300

    banner grabber modification

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -15,7 +15,7 @@ def banner(ip, port):
     s = socket.socket()
     s.connect((ip, int(port)))
     s.settimeout(5)
-    print(str(s.recv(1024)))
+    print(str(s.recv(1024)).strip('\n', 'r', 'b'))
     
 def main():
     ip = input("Please enter the IP: ")
