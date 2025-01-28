47bee9d8452ff137a65d95e18f9b9b3b168ac669
lu-dwig <murimiludwig@gmail.com>
2025-01-28 21:50:09 +0300

    readme.md file creation

--- BannerGrabber.py
+++ BannerGrabber.py
@@ -15,7 +15,7 @@ def banner(ip, port):
     s = socket.socket()
     s.connect((ip, int(port)))
     s.settimeout(5)
-    print(s.recv(1024))
+    print(str(s.recv(1024)))
     
 def main():
     ip = input("Please enter the IP: ")
